9ed8d8eed46213e8a452f8ac76706e8c70920f33
levi-w-lucy <reachfan169@gmail.com>
2024-04-24 08:10:41 -0500

    Profit Calculator Finished Project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/levi-w-lucy/ProfitCalculator
+
+go 1.22.2
--- profit_calculator.go
+++ profit_calculator.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello world")
+	var revenue, expenses, taxRate float64
+
+	retreiveValFromUser("Enter your revenue: ", &revenue)
+	retreiveValFromUser("Enter your total expenses: ", &expenses)
+	retreiveValFromUser("Enter your tax rate: ", &taxRate)
+
+	earningsBeforeTax := revenue - expenses
+	profit := earningsBeforeTax * (1 - taxRate/100)
+	fmt.Println("Your earnings before tax is $" + fmt.Sprintf("%.2f", earningsBeforeTax))
+	fmt.Println("Your earnings after tax is $" + fmt.Sprintf("%.2f", profit))
+	fmt.Println("Your EBT to Profit Ratio is " + fmt.Sprintf("%.2f", earningsBeforeTax/profit))
+}
+
+func retreiveValFromUser(message string, variablePtr *float64) {
+	fmt.Print(message)
+	fmt.Scan(variablePtr)
 }
