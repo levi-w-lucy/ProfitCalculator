8ee9ec379fd257cfab3e54367451502a04e82562
levi-w-lucy <reachfan169@gmail.com>
2024-04-28 08:08:15 -0500

    Split out the financial data calculations to a separate function

--- profit_calculator.go
+++ profit_calculator.go
@@ -9,14 +9,20 @@ func main() {
 	retreiveValFromUser("Enter your total expenses: ", &expenses)
 	retreiveValFromUser("Enter your tax rate: ", &taxRate)
 
-	earningsBeforeTax := revenue - expenses
-	profit := earningsBeforeTax * (1 - taxRate/100)
+	earningsBeforeTax, profit, ebtProfitRatio := getFinancialData(revenue, expenses, taxRate)
 	fmt.Println("Your earnings before tax is $" + fmt.Sprintf("%.2f", earningsBeforeTax))
 	fmt.Println("Your earnings after tax is $" + fmt.Sprintf("%.2f", profit))
-	fmt.Println("Your EBT to Profit Ratio is " + fmt.Sprintf("%.2f", earningsBeforeTax/profit))
+	fmt.Println("Your EBT to Profit Ratio is " + fmt.Sprintf("%.2f", ebtProfitRatio))
 }
 
 func retreiveValFromUser(message string, variablePtr *float64) {
 	fmt.Print(message)
 	fmt.Scan(variablePtr)
+}
+
+func getFinancialData(revenue, expenses, taxRate float64) (earningsBeforeTax, profit, ebtProfitRatio float64) {
+	earningsBeforeTax = revenue - expenses
+	profit = earningsBeforeTax * (1 - taxRate/100)
+	ebtProfitRatio = earningsBeforeTax / profit
+	return
 }
