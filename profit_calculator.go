e8ac38fd5ccaf746affb4e3e246ec39b83de34c3
levi-w-lucy <reachfan169@gmail.com>
2024-05-02 05:21:13 -0700

    Added input validation, and output writing to a text file.

--- profit_calculator.go
+++ profit_calculator.go
@@ -1,23 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const financialDataFile = "financialData.txt"
 
 func main() {
 	var revenue, expenses, taxRate float64
 
-	retreiveValFromUser("Enter your revenue: ", &revenue)
-	retreiveValFromUser("Enter your total expenses: ", &expenses)
-	retreiveValFromUser("Enter your tax rate: ", &taxRate)
+	revenue = retreiveValFromUser("Enter your revenue: ")
+	expenses = retreiveValFromUser("Enter your total expenses: ")
+	taxRate = retreiveValFromUser("Enter your tax rate: ")
 
 	earningsBeforeTax, profit, ebtProfitRatio := getFinancialData(revenue, expenses, taxRate)
-	fmt.Println("Your earnings before tax is $" + fmt.Sprintf("%.2f", earningsBeforeTax))
-	fmt.Println("Your earnings after tax is $" + fmt.Sprintf("%.2f", profit))
-	fmt.Println("Your EBT to Profit Ratio is " + fmt.Sprintf("%.2f", ebtProfitRatio))
+
+	ebtString := "Your earnings before tax is $" + fmt.Sprintf("%.2f", earningsBeforeTax)
+	earningsString := "Your earnings after tax is $" + fmt.Sprintf("%.2f", profit)
+	ebtRatio := "Your EBT to Profit Ratio is " + fmt.Sprintf("%.2f", ebtProfitRatio)
+
+	fmt.Println(ebtString)
+	fmt.Println(earningsString)
+	fmt.Println(ebtRatio)
+
+	writeFinancialData(fmt.Sprintf("%s\n%s\n%s", ebtString, earningsString, ebtRatio))
 }
 
-func retreiveValFromUser(message string, variablePtr *float64) {
+func retreiveValFromUser(message string) float64 {
+	var inputVal float64
+
 	fmt.Print(message)
-	fmt.Scan(variablePtr)
+	fmt.Scan(&inputVal)
+	validateNumberInput(inputVal)
+
+	return inputVal
+}
+
+func validateNumberInput(input float64) {
+	if input <= 0 {
+		panic("Input value was invalid. Value should be greater than 0")
+	}
 }
 
 func getFinancialData(revenue, expenses, taxRate float64) (earningsBeforeTax, profit, ebtProfitRatio float64) {
@@ -25,4 +48,8 @@ func getFinancialData(revenue, expenses, taxRate float64) (earningsBeforeTax, pr
 	profit = earningsBeforeTax * (1 - taxRate/100)
 	ebtProfitRatio = earningsBeforeTax / profit
 	return
+}
+
+func writeFinancialData(financialData string) {
+	os.WriteFile(financialDataFile, []byte(financialData), 0644)
 }
